92ad0dcea757d05aa9b5e30df270e7b6dd7adcc1
wmancaniello <wmancaniello@gmail.com>
2024-05-21 19:14:19 +0200

    Add filter parking and README

---
+++ README.md
@@ -0,0 +1,22 @@
+
+**`nome repo: php-hotel`**
+
+### CONSEGNA
+**Array di Hotel:**
+https://www.codepile.net/pile/OEWY7Q1G
+
+---
+
+1) Stampare tutti i nostri hotel con tutti i dati disponibili.
+    - Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+    - Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+---
+
+2) Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+
+---
+
+3) Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+
+---
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
+// Array Hotel
 $hotels = [
-
     [
         'name' => 'Hotel Belvedere',
         'description' => 'Hotel Belvedere Descrizione',
@@ -37,32 +37,53 @@ $hotels = [
         'vote' => 2,
         'distance_to_center' => 50
     ],
-
 ];
 
+// Array Hotel filtrati
+$filter_hotels = $hotels;
+
+// Filtro presenza parcheggio
+// Se è vera filtra gli Hotel
+if ($_GET['parking'] ?? false) {
+    $filter_hotels = array_filter($filter_hotels, function ($hotel) {
+        return $hotel['parking'] === true;
+    });
+}
+
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lista Hotel</title>
 
-    <!-- CDN BOOTSTRAP -->
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <!-- FONTAWESOME -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
 <body>
     <div class="container mt-5">
-        <h1 class="text-center">Lista HOTEL</h1>
+        <h1 class="text-center mb-4">Lista Hotel</h1>
 
+        <!-- Form per il filtro del parcheggio -->
+        <form method="GET" class="d-flex flex-column">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="parking" value="1" id="parkingCheck" <?php echo ($_GET['parking'] ?? '') === '1' ? 'checked' : ''; ?>>
+                <label class="form-check-label" for="parkingCheck">
+                    Parcheggio
+                </label>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+                <button type="submit" class="btn btn-primary col-2 ">Filtra</button>
+            </div>
+        </form>
+
+        <!-- Tabella degli hotel -->
         <table class="table table-striped">
 
             <thead>
@@ -76,20 +97,15 @@ $hotels = [
             </thead>
 
             <tbody>
-                <?php foreach ($hotels as $hotel) : ?>
+                <?php foreach ($filter_hotels as $hotel) : ?>
                     <tr>
                         <td><?= $hotel['name'] ?></td>
                         <td><?= $hotel['description'] ?></td>
                         <td><?= $hotel['parking'] ? 'Presente' : 'Non presente' ?></td>
                         <td>
-                            <?php
-
-                            $num_stelle = $hotel['vote'];
-                            for ($i = 0; $i < $num_stelle; $i++) {
-                                echo '<i class="fa-solid fa-star"></i>';
-                            }
-
-                            ?>
+                            <?php for ($i = 0; $i < $hotel['vote']; $i++) : ?>
+                                <i class="fa-solid fa-star"></i>
+                            <?php endfor; ?>
                         </td>
                         <td><?= $hotel['distance_to_center'] ?></td>
                     </tr>
